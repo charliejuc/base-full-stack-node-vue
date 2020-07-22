59bfa3a1e0a46026f14af5e5e274523a34acb7ba
charliejuc <charliejuc@gmail.com>
2020-07-22 15:40:06 +0200

    Rewrites mock.

--- backend/src/tests/module/quiz/application/CreateQuizSuccess.spec.ts
+++ backend/src/tests/module/quiz/application/CreateQuizSuccess.spec.ts
@@ -1,22 +1,27 @@
 import { CreateQuizUseCase } from '@/module/quiz/application/CreateQuizUseCase'
 import { createQuizUseCaseMother } from '../domain/mother/CreateQuizUseCaseMother'
 
+let createQuizUseCase: CreateQuizUseCase
+let createQuizUseCaseMock: {
+    handle: jest.Mock
+}
+
+beforeEach(() => {
+    createQuizUseCase = createQuizUseCaseMother()
+    createQuizUseCaseMock = {
+        handle: jest.fn(createQuizUseCase.handle)
+    }
+})
+
 test('CreateQuizUseCase exist', () => {
     expect(CreateQuizUseCase).toBeTruthy()
 })
 
 test('should be build CreateQuizUseCase instance', () => {
-    const createQuizUseCase = createQuizUseCaseMother()
-
     expect(createQuizUseCase).toBeTruthy()
 })
 
-test('CreateQuizUseCase handle should not throw an exception', async () => {
-    const createQuizUseCase = createQuizUseCaseMother()
-    const createQuizUseCaseMock = {
-        handle: jest.fn(createQuizUseCase.handle)
-    }
-
+test('CreateQuizUseCase handle should not throw an exception', () => {
     createQuizUseCaseMock.handle()
 
     expect(createQuizUseCaseMock.handle.mock.calls.length).toBe(1)
