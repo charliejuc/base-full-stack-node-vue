72d50f32f9701da530c5b724318535bd67295044
charliejuc <charlieinsous@gmail.com>
2020-07-24 09:19:03 +0200

    Maps modules starting with "@" alias for jest.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
     transform: {
         '^.+\\.ts$': 'ts-jest',
         '^.+\\.vue$': 'vue-jest'
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': path.join(__dirname, 'src', '$1')
     }
 }
